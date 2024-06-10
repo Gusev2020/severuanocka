1354a52fcb9c1460a83434c5037cc73b7aa2272a
Gusev2020 <gusev12.gusev@yandex.ru>
2024-06-10 14:58:05 +0300

    Компонент типографики

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import typography from '@/shared/typography/index.vue'
+</script>
+
+<template>
+  <main>
+    <typography tagName="p" size="xl" :bold="true">test</typography>
+  </main>
+</template>
---
+++ src/widgets/TheHeader.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+<template>
+  <header class="header"></header>
+</template>
+<style scoped></style>
